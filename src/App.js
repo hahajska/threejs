2d44ecbf1e4bed6b014ee1e8dd40146917b69f3b
TomasSchlogl <thomas537@seznam.cz>
2021-05-11 14:58:23 +0200

    dom, gsap, motion added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import "./App.css";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App h-screen bg-gray">
-      <h1>hello</h1>
+    <div className="App h-screen bg-blue-50 flex justify-center items-center">
+      <div className="h-7 w-24 bg-blue-200 flex justify-center items-center rounded-md hover:bg-blue-600 cursor-pointer">
+        <h1>EXPLORE</h1>
+      </div>
     </div>
   );
 }
